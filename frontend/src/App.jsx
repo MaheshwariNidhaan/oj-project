f8b4874377317fad15cc94ab534a952b366f0b69
MaheshwariNidhaan <133689983+MaheshwariNidhaan@users.noreply.github.com>
2024-05-26 18:50:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import About from "./components/About";
-// import Problems from "./components/Problems";
-// import Leaderboard from "./components/Leaderboard";
-// import Login from "./components/Login";
-// import Signup from "./components/Signup";
-// import ProtectedRoute from "./components/ProtectedRoute";
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div>
-//         <Navbar />
-//         <div className="content">
-//           <Routes>
-//             <Route path="/login" element={<Login />} />
-//             <Route path="/signup" element={<Signup />} />
-
-//             <Route path="/" element={<About />} />
-//             <Route path="/problems" element={<Problems />} />
-//             <Route path="/leaderboard" element={<Leaderboard />} />
-//           </Routes>
-//         </div>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./components/About";
